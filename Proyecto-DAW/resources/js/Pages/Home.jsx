152db603a3f9c6e80773f79444a4e6bd28bed594
MarioDSalamanca <dieguezmario07@gmail.com>
2024-04-09 10:42:14 +0200

    a

--- Proyecto-DAW/resources/js/Pages/Home.jsx
+++ Proyecto-DAW/resources/js/Pages/Home.jsx
@@ -6,12 +6,12 @@ export default function Home({ sesionUsuario }) {
             <Header sesion={ sesionUsuario }/>
             <main>
                 <div className="home">
-                    <a className="link-home" href="/productos">Productos</a>
-                    <a className="link-home" href="/ventas">Ventas</a>
-                    <a className="link-home" href="/compras">Compras</a>
-                    <a className="link-home" href="/proveedores">Proveedores</a>
-                    <a className="link-home" href="/empleados">Empleados</a>
-                    <a className="link-home" href="/tareas">Tareas</a>
+                    <a className="link-home productos" href="/productos"><span>Productos</span></a>
+                    <a className="link-home ventas" href="/ventas"><span>Ventas</span></a>
+                    <a className="link-home compras" href="/compras"><span>Compras</span></a>
+                    <a className="link-home proveedores" href="/proveedores"><span>Proveedores</span></a>
+                    <a className="link-home empleados" href="/empleados"><span>Empleados</span></a>
+                    <a className="link-home tareas" href="/tareas"><span>Tareas</span></a>
                 </div>
             </main>
         </>
