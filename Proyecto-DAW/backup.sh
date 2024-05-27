01e982c3ffd9c2dd537b5764e468600407bb9714
MarioDSalamanca <dieguezmario07@gmail.com>
2024-05-27 09:14:47 +0200

    a

--- Proyecto-DAW/backup.sh
+++ Proyecto-DAW/backup.sh
@@ -6,10 +6,14 @@
     DB_PASSWORD="rootroot"
     DB_NAME="ERP"
     BACKUP_DIR="./backups/"
-    TIMESTAMP=$(date +"%Y%m%d%H%M")
+    TIMESTAMP=$(date +"%d-%m-%Y")
 
 # Crear el respaldo
     mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql
 
 # Opcional: Eliminar respaldos antiguos (ej. más de 7 días)
     find $BACKUP_DIR -type f -name "*.sql" -mtime +3 -exec rm {} \;
+
+# Pasos para automatizar
+    # crontab -e
+    # 0 1 * * * /var/www/html/ruta del proyecto/backup.sh ( se ejecuta todos los días a la 1 am )
