a68f8d9038b9d0d0eaa41d92ff55b92067a52c1d
MarioDSalamanca <dieguezmario07@gmail.com>
2024-06-02 11:59:49 +0200

    a

--- Proyecto-DAW/backup.sh
+++ Proyecto-DAW/backup.sh
@@ -8,12 +8,14 @@
     BACKUP_DIR="./backups/"
     TIMESTAMP=$(date +"%d-%m-%Y")
 
-# Crear el respaldo
+# Dump a la base de datos (copia)
     mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql
 
-# Opcional: Eliminar respaldos antiguos (ej. más de 7 días)
+# Eliminar respaldos antiguos de más de 3 días
     find $BACKUP_DIR -type f -name "*.sql" -mtime +3 -exec rm {} \;
 
 # Pasos para automatizar
+    # El demonio cron de Ubuntu para automatizar tareas
     # crontab -e
-    # 0 1 * * * /var/www/html/ruta del proyecto/backup.sh ( se ejecuta todos los días a la 1 am )
+    # Se ejecuta todos los días a la 1 am
+    # 0 1 * * * /var/www/html/ruta del proyecto/backup.sh
